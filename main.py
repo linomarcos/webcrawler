ffdc7c6c4fb7f6500e0c0b6dae20273d1f31226b
Jerry Gagelman <gagelman@gmail.com>
2014-09-07 22:45:39 -0400

    adds basic CL driver and config file for webgraph crawler

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import yaml
+import logging
+import argparse
+import beanstalkc
+from crawler_node import CrawlerNode
+from webgraph.crawl_api import CrawlAPI
+
+ROOT_PATH = os.path.dirname(__file__)
+DEFAULT_CONFIG = os.path.join(ROOT_PATH, 'config.yaml')
+
+def seed_queue(config, seedfile):
+    client = beanstalkc.Connection(**config['beanstalk_config'])
+    print 'connected at %s' % config['beanstalk_config']
+
+    print 'reading from seed file %s' % seedfile
+    for url in map(str.strip, open(args.seedfile)):
+        jobdefn = dict(url=url, depth=0)
+        client.put(json.dumps(jobdefn))
+
+
+parser = argparse.ArgumentParser(description='Webcrawler interface')
+parser.add_argument('--config', type=str, default=DEFAULT_CONFIG)
+parser.add_argument('--seedfile', type=str, help='list of seed URLs, one per line.')
+parser.add_argument('--crawl', action='store_true')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    config = yaml.load(open(args.config))
+    logging.basicConfig(**config['logging_config'])
+
+    if args.seedfile:
+        seed_queue(config, args.seedfile)
+
+    if args.crawl:
+        api = CrawlAPI()
+        worker = CrawlerNode(api, args.config)
+        worker.start()
+
